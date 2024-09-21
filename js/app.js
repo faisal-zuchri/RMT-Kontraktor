1fc274cc5c86cb20763629ad0094eed7abf61d1c
FaisalZuchri <akun.zuchree@gmail.com>
2024-09-21 15:43:49 +0700

    push

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
 import {} from "./navbar-bottom.js";
+
+let stat = document.querySelectorAll(".stat");
+let infoStat = document.querySelector(".info-stat");
+
+let test = false;
+
+window.onscroll = () => {
+  if ((window.screenY = infoStat.offsetTop)) {
+    if (!test) {
+      stat.forEach((e) => {
+        let start = 0;
+        let end = e.dataset.num;
+
+        let count = setInterval(() => {
+          start++;
+          e.textContent = start;
+          if (start == end) {
+            clearInterval(count);
+          }
+        }, 2000 / end);
+      });
+    }
+  }
+};
